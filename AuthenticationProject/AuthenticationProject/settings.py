ed10c3fc445246c2fe1bbc04a623e6bd357b0bbb
Josue <kazimotojosue@gmail.com>
2026-05-25 16:49:06 +0200

    Ajout de la gestion des variables d'environnement dans settings.py, mise à jour du fichier .gitignore pour inclure .env, et création de la vue Home avec une route correspondante.

--- AuthenticationProject/AuthenticationProject/settings.py
+++ AuthenticationProject/AuthenticationProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w)wz#d%@@f+tl3u=fs3@l^ov9txefg+-4)irwj160f!6nn$dsy'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
