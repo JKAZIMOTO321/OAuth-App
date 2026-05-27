a0fbe3905e0da319c8ac0a91aaa69a90fa656d0f
Josue <kazimotojosue@gmail.com>
2026-05-27 09:26:03 +0200

    Correction de l'importation de la vue 'Home' dans le fichier urls.py et ajout de la route pour 'google_login'.

--- AuthenticationProject/Core/urls.py
+++ AuthenticationProject/Core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import Home
+from . import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 urlpatterns = [
-    path('', Home, name='home'),
+    path('', views.Home, name='home'),
+    path('google_login/', views.google_auth, name='google_login'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
