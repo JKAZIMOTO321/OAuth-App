7cabee0ccf54534898c304aa4e5a2dc667d14ce0
Josue <kazimotojosue@gmail.com>
2026-05-27 09:26:16 +0200

    Ajout de la configuration CORS et mise à jour des paramètres OAuth dans settings.py

--- AuthenticationProject/AuthenticationProject/settings.py
+++ AuthenticationProject/AuthenticationProject/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'Core',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +132,6 @@ REST_FRAMEWORK = {
 # Configuration for Google OAuth
 GOOGLE_OAUTH_CLIENT_ID = config('GOOGLE_OAUTH_CLIENT_ID')
 GOOGLE_OAUTH_CLIENT_SECRET = config('GOOGLE_OAUTH_CLIENT_SECRET')
+
+# Configuration for CORS
+CORS_ALLOW_ALL_ORIGINS = True
